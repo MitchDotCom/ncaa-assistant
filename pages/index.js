ad11e70b8b36a45883a350bbb1a598ff48e8a1fc
MitchDotCom <mitchleblanc3@gmail.com>
2025-08-24 12:27:44 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>NHL CBA Assistant</title>
+        <title>NCAA Recruiting & Compliance Assistant</title>
       </Head>
       <main>
-        <h1 style={{ textAlign: 'center' }}>Ask the NHL CBA Assistant</h1>
+        <h1 style={{ textAlign: 'center' }}>Ask the NCAA Recruiting & Compliance Assistant</h1>
         <ChatWindow />
       </main>
     </>
